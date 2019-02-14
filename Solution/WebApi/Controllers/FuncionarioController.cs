65e20ca1e9e88dddd82e98d2c3fa578f587f7a8d
Marcelo Santos Dornelas <msdornelas@hotmail.com>
2019-02-13 23:11:02 -0200

    Adicionados os arquivos da solution WebApi.

---
+++ Solution/WebApi/Controllers/FuncionarioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class FuncionarioController : ApiController
+    {
+        // Lista de funcionários criada para testes com a API
+        public Funcionario[] funcionarios = new Funcionario[]
+        {
+            new Funcionario { Id = 1, Nome = "Marcelo Dornelas", Cargo = "Power Ranger Green" },
+            new Funcionario { Id = 2, Nome = "Luciano Fernandes", Cargo = "Power Ranger Red" },
+            new Funcionario { Id = 3, Nome = "Vinícius Gonzalez", Cargo = "Power Ranger Blue" },
+            new Funcionario { Id = 4, Nome = "Lucas Gomes", Cargo = "Power Ranger Yellow" },
+            new Funcionario { Id = 5, Nome = "Lucas Botaro", Cargo = "Power Ranger Pink" },
+            new Funcionario { Id = 6, Nome = "Igor Abdallah", Cargo = "Power Ranger Black" }
+        };
+
+        // GET: api/Funcionario
+        public IEnumerable<Funcionario> Get()
+        {
+            return funcionarios;
+        }
+
+        // GET: api/Funcionario/5
+        public IHttpActionResult Get(int id)
+        {
+            var funcionario = funcionarios.FirstOrDefault(item => item.Id == id);
+
+            if (funcionario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(funcionario);
+        }
+
+    }
+}
---
+++ Solution/WebApi/Models/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cargo { get; set; }
+    }
+}
